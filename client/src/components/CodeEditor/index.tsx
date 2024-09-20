e0e9d916b0aff2bfc1b5a588b14325fee122c53f
Palash <ng.palash.das@gmail.com>
2024-09-20 17:30:43 +0530

    Update package dependencies and add new components

--- client/src/components/CodeEditor/index.tsx
+++ client/src/components/CodeEditor/index.tsx
@@ -1,12 +1,11 @@
+"use client";
 import React from "react";
+import Monaco from "@monaco-editor/react";
 
 const CodeEditor: React.FC = () => {
   return (
-    <div className="w-full h-3/4 bg-gray-900 text-white p-4">
-      <textarea
-        className="w-full h-full bg-gray-900 text-green-400 font-mono p-2"
-        defaultValue={`// Your code goes here\n\nconst greeting = "Hello, World!";\nconsole.log(greeting);`}
-      />
+    <div className="w-full h-3/4 bg-vscode text-white">
+      <Monaco language="javascript" theme="vs-dark" />
     </div>
   );
 };
